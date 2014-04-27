6631b5f5cee6defddf358c12bab0c7da6380cabf
Carl Palowitch <carl.palowitch@gmail.com>
2014-04-27 15:04:17 -0700

    added header and basic vector functions

--- cachematrix.R
+++ cachematrix.R
@@ -1,3 +1,4 @@
+## Last changed 4/27 2:57
 ## Put comments here that give an overall description of what your
 ## functions do
 
@@ -12,4 +13,32 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+}
+
+## Here is the makeVector function to work with
+makeVector <- function(x = numeric()) {
+        m <- NULL
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x
+        setmean <- function(mean) m <<- mean
+        getmean <- function() m
+        list(set = set, get = get,
+             setmean = setmean,
+             getmean = getmean)
+}
+
+## Here is the cachemean function to work with
+cachemean <- function(x, ...) {
+        m <- x$getmean()
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        data <- x$get()
+        m <- mean(data, ...)
+        x$setmean(m)
+        m
 }
